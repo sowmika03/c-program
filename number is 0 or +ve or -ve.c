3b851297b0f294f5d28dec79357fd8584288be00
sowmika03 <vemulasaisowmika@gmail.com>
2024-10-25 12:22:20 +0530

    Create number is 0 or +ve or -ve.c

---
+++ number is 0 or +ve or -ve.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main() 
+{
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive");
+    }
+    else if(n==0)
+    {
+        printf ("zero");
+    }
+    else
+    {
+        printf("negative");
+    }
+}
