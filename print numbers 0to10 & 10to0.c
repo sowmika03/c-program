591632fbcefb747c0703c1c2b2cec75cb5ed3067
sowmika03 <vemulasaisowmika@gmail.com>
2024-12-20 12:02:40 +0530

    Create print numbers 0to10 & 10to0.c

---
+++ print numbers 0to10 & 10to0.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+    int n=0,a=10;
+    while(n<=10)
+    {
+      printf("%d",n);
+      n++;  
+    }
+    printf("\n");
+    while(a>=0)
+    {
+      printf("%d",a);
+      a--;  
+    }
+
+    return 0;
+}
