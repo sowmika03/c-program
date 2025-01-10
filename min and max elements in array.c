c99c9ba2d538a5dedceb2fe861f14b64208b00fb
sowmika03 <vemulasaisowmika@gmail.com>
2025-01-10 11:29:10 +0530

    Create min and max elements in array.c

---
+++ min and max elements in array.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i,max,min;
+    printf("enter the size:");
+    scanf("%d",&n);
+    printf("enter the elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    min=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+        if(a[i]<min)
+        {
+            min=a[i];
+        }
+    }
+    printf("max element is: %d \n",max);
+    printf("min element is: %d",min);
+    return 0;
+}
