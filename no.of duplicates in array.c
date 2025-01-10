89454153b7ca586ff0e9ec5f7255aafb1fae9b63
sowmika03 <vemulasaisowmika@gmail.com>
2025-01-10 11:40:26 +0530

    Create no.of duplicates in array.c

---
+++ no.of duplicates in array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i,j,count=0;
+    printf("enter the size:");
+    scanf("%d",&n);
+    printf("enter the elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    printf("the no.of duplicate elements are: %d",count);
+    return 0;
+}
