75c77ca27a439246602511a57a3e855714e4898f
sowmika03 <vemulasaisowmika@gmail.com>
2025-01-10 11:11:01 +0530

    Create read and print elements of array.c

---
+++ read and print elements of array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,i;
+    printf("enter the size:");
+    scanf("%d",&n);
+    printf("enter the elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    return 0;
+}
